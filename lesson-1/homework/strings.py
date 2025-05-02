66af180fd1848a7a77171ae3d5991af3277e6bda
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-02 18:56:57 +0500

    Update strings.py

--- lesson-1/homework/strings.py
+++ lesson-1/homework/strings.py
@@ -1,1 +1,16 @@
-print('Lorem ipsum dolor sit amet')
+#Given a side of square. Find its perimeter and area.
+side = 5
+p = 4 * side
+s = side ** 2
+print (f'Perimeter: {p}\nArea: {s}')
+#Given diameter of circle. Find its length.
+diameter = 4
+print (f'The length of the circle is {4*3.14}')
+#Given two numbers a and b. Find their mean.
+a = 6
+b = 2
+print(f'The mean is: {(a+b)/2}')
+#Given two numbers a and b. Find their sum, product and square of each number.
+a = 16
+b = 25
+print(f'The sum is: {a+b}\nThe product is: {a*b}\nThe square of a is: {a**2}\nThe square of b is: {b**2}')
