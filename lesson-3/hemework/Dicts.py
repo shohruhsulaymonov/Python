6ae12d388a39d15e3b66686630e1ea3995c8ff74
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-07 12:20:21 +0500

    Update Dicts.py

--- lesson-3/hemework/Dicts.py
+++ lesson-3/hemework/Dicts.py
@@ -1,1 +1,14 @@
-
+#1. Create and Access List Elements
+list = ['Apple', 'Orange', "Pineapple", 'Banana', 'Pomegranate']
+print(list[2 ])
+#2. Concatenate Two Lists
+list1 = ['Potato', 'Cucumber', 'Corn', 'Tomato']
+list2 = ['Apple', 'Orange', "Pineapple", 'Banana', 'Pomegranate']
+concatlists = list1 + list2
+#3. Extract Elements from a List
+elements = ['Interstellar', 'Lalaland', 'Corn', 'Tomato', 'Banana']
+print(elements[1])
+#4. Convert List to Tuple
+movies = ['Interstellar', 'Lalaland', 'Inception', 'Groundhog Day', '8 Mile']
+tpmovies = tuple(movies)
+print(tpmovies[1])
