5dc0db8948baae11ea22dfd8a2c7205b3eaa2b7e
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-08 11:08:55 +0500

    Update Dicts.py

--- lesson-3/hemework/Dicts.py
+++ lesson-3/hemework/Dicts.py
@@ -1,13 +1,17 @@
 #1. Create and Access List Elements
 list = ['Apple', 'Orange', "Pineapple", 'Banana', 'Pomegranate']
-print(list[2 ])
+print(list[2])
 #2. Concatenate Two Lists
 list1 = ['Potato', 'Cucumber', 'Corn', 'Tomato']
 list2 = ['Apple', 'Orange', "Pineapple", 'Banana', 'Pomegranate']
 concatlists = list1 + list2
 #3. Extract Elements from a List
 elements = ['Interstellar', 'Lalaland', 'Corn', 'Tomato', 'Banana']
-print(elements[1])
+first = elements[0]
+middle = elements[2]
+last = elements[-1]
+newlist = [first, middle, last]
+print(newlist)
 #4. Convert List to Tuple
 movies = ['Interstellar', 'Lalaland', 'Inception', 'Groundhog Day', '8 Mile']
 tpmovies = tuple(movies)
@@ -18,3 +22,43 @@ print('Paris' in lst)
 #6. Duplicate a List Without Using Loops
 lst = [1, 4, 2, 7, 2, 6, 8, 2, 0, 2, 1, 0]
 print(lst * 2)
+#7. Swap First and Last Elements of a List
+elements = ['Interstellar', 'Lalaland', 'Corn', 'Tomato', 'Banana']
+first = elements[0]
+last = elements[-1]
+middle = first
+first = last
+last = middle
+elements[0] = first
+elements[-1] = last
+print(elements)
+#8. Slice a Tuple
+num = list(range(1, 11))
+print(num[3:7])
+#9. Count Occurrences in a List
+colors = ['Blue', 'Red', 'Yellow', 'Blue', 'Green', 'Blue']
+print(colors.count('Blue'))
+#10. Find the Index of an Element in a Tuple
+animals = ('lion', 'Hawk', 'Cat', 'Bear', 'Wolf', 'Slime')
+print(animals.index('lion'))
+#11. Merge Two Tuples
+colors = ('Blue', 'Red', 'Yellow', 'Blue', 'Green', 'Blue')
+animals = ('lion', 'Hawk', 'Cat', 'Bear', 'Wolf', 'Slime')
+anicolors = colors + animals
+print(anicolors)
+#12. Find the Length of a List and Tuple
+colors = ['Blue', 'Red', 'Yellow', 'Blue', 'Green', 'Blue']
+animals = ('lion', 'Hawk', 'Cat', 'Bear', 'Wolf', 'Slime')
+print(len(colors))
+print(len(animals))
+#13. Convert Tuple to List
+numbers = (12, 3, 9, 98 ,93)
+numbers = list(numbers)
+print(numbers)
+#14. Find Maximum and Minimum in a Tuple
+numbers = (12, 3, 9, -98 ,93)
+print(f'Max: {max(numbers)}')
+print(f'Min: {min(numbers)}')
+#15. Reverse a Tuple
+colors = ('Blue', 'Red', 'Yellow', 'Blue', 'Green', 'Blue')
+print(colors[::-1])
