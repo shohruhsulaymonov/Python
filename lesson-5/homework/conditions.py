19fce5b1f89c7f2bb40f79fd88308bd05807a59e
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-12 16:14:34 +0500

    Update conditions.py

--- lesson-5/homework/conditions.py
+++ lesson-5/homework/conditions.py
@@ -12,3 +12,7 @@ if (n % 2 != 0) or (n % 2 == 0 and 6 <= n <= 20):
     print('Weird')
 else:
     print('Not weird')
+#3. Given two integer numbers a and b. Find even numbers between this numbers. a and b are inclusive. Don't use loop.
+#Solution 1 with if-else statement.
+
+#Solution 2 without if-else statement.
