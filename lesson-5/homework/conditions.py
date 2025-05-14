59d5d945f5ceb0766170194bfe303734b62b5a19
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-14 12:40:10 +0500

    Update conditions.py

--- lesson-5/homework/conditions.py
+++ lesson-5/homework/conditions.py
@@ -8,12 +8,6 @@ def is_leap(year):
         print(False)
 is_leap(2025)
 #2. Conditional Statements Exercise
-n = int(input())
-if (n % 2 != 0) or (n % 2 == 0 and n in range(6, 21)):
-    print('Weird')
-else:
-    print('Not weird')
-#2. Conditional Statements Exercise
 n = int(input('Input an integer bewtween 1 and 100: '))
 while not 1 <= n <= 100:
     n = int(input('Input an integer bewtween 1 and 100: '))
