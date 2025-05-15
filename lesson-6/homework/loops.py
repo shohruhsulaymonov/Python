576b494217ac4d6c88a825dbe694fbb0491fce82
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-15 11:31:56 +0500

    Create loops.py

---
+++ lesson-6/homework/loops.py
@@ -0,0 +1,38 @@
+
+
+
+#2
+while True:
+    n = int(input('Input a number: '))
+    if 1 <= n <= 20:
+        break
+    else:
+        print('Input a number between 1 and 20')
+for i in range(n):
+    print(i**2)
+#3 Loop-Based Exercises
+#Exercise 1: Print first 10 natural numbers using a while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+#Exercise 2: Print the following pattern
+n = 5
+
+for i in range(1, n+1):
+    
+    for j in range(1, i+1):
+        print(j, end = ' ')
+
+    print(end ='\n')
+#Exercise 3: Calculate sum of all numbers from 1 to a given number
+n = int(input('Enter a number: '))
+s = 0
+for i in range(1, n+1):
+    s += i
+print(f'Sum is: {s}')
+#Exercise 4: Print multiplication table of a given number
+n = int(input('Enter a number: '))
+for i in range(1, 11):
+    print(n*i)
+#Exercise 5: Display numbers from a list using a loop
