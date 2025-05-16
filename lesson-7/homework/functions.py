c08e13483c7d642f29c3ce0a02cdd64da55bff6d
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-16 16:38:20 +0500

    Update functions.py

--- lesson-7/homework/functions.py
+++ lesson-7/homework/functions.py
@@ -1,6 +1,20 @@
 #1
-
-
+def is_prime(n):
+    if n < 2:
+        while True:
+            n = int(input('Enter a natural number above 1: '))
+            if n > 1:
+                break
+    
+    divisors = 0
+    for i in range(2, n):
+        if n % i == 0:
+            divisors += 1
+    if divisors == 0:
+        print(True)
+    else:
+        print(False)
+is_prime(0)
 #2
 def digitsum(k) -> int:
     x = str(k)
