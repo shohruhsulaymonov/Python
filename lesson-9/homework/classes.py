85b5ec788f9cc3a126359741158bb085d2992813
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-25 19:26:06 +0500

    Update classes.py

--- lesson-9/homework/classes.py
+++ lesson-9/homework/classes.py
@@ -18,3 +18,23 @@ print('The perimeter of the cirle is:',  medium_circle.perimeter())
 
 print('The area of the cirle is:',  large_circle.area())
 print('The perimeter of the cirle is:',  large_circle.perimeter())
+#2
+from datetime import date, datetime
+
+class Person:
+
+    def __init__(self, name, country, date_of_birth):
+        self.name = name
+        self.country = country
+        self.date_of_birth = date_of_birth
+
+    def get_age(self):
+        today = date.today()
+        dob = datetime.strptime(self.date_of_birth,"%Y-%m-%d").date()
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        
+        return age
+
+
+client = Person('Nice', 'Uzbekistan', '2007-05-30')
+print(client.get_age())
