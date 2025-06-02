efe87edc8292d4f6f8edee53e9d8759fe9c0604e
Reinhardt <shsulaymonov2007@gmail.com>
2025-06-02 19:28:26 +0500

    Update classes.py

--- lesson-9/homework/classes.py
+++ lesson-9/homework/classes.py
@@ -178,6 +178,133 @@ class Stack:
     def size(self):
         return len(self.elements_list)
 #7
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def InsAtBegin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+    def InsAtIndex(self, data, index):
+        if index == 0:
+            self.InsAtBegin(data)
+            return
+        
+        position = 0
+        current_node = self.head
+        while current_node is not None and position + 1 != index:
+            position += 1
+            current_node = current_node.next
+
+        if current_node is not None:
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node
+        else:
+            print('Invalid index')
+        
+    
+    def InsAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+
+        current_node.next = new_node
+
+
+    def UpdateNode(self, value, index):
+        current_node = self.head
+        position = 0
+        
+        while current_node is not None and position != index:
+            position += 1
+            current_node = current_node.next
+        if current_node is not None:
+            current_node.data = value
+        else:
+            print('Index not present')
+
+    def remove_first_node(self):
+        if self.head is None:
+            return
+        
+        self.head = self.head.next
+
+    def remove_last_node(self):
+        if self.head is None:
+            return
+
+        current_node = self.head
+        while current_node.next is not None and current_node.next.next is not None:
+            current_node = current_node.next
+        
+        current_node.next = None
+
+    def remove_at_index(self, index):
+        if self.head is None:
+            return
+            
+        current_node = self.head
+        position = 0
+
+        if index == 0:
+            self.remove_first_node()
+        else:
+            while current_node.next is not None and position + 1 != index:
+                position += 1
+                current_node = current_node.next
+
+            if current_node is None or current_node.next is None:
+                print("Index not present")
+            else:
+                current_node.next = current_node.next.next
+        
+    def remove_node(self, data):
+        current_node = self.head
+        
+        if current_node.data == data:
+            self.remove_first_node()
+            return
+        
+        while current_node is not None and current_node.next.next.data != data:
+            current_node = current_node.next
+
+        if current_node is None:
+            print('The data does not present')
+        else:
+            current_node.next = current_node.next.next
+
+    def printLL(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def sizeOfLL(self):
+        size = 0
+        if self.head is None:
+            return 0
+        current_node = self.head
+        while current_node is not None:
+            size += 1
+            current_node = current_node.next 
+        return size 
+
 #8
 class Cart:
     items = dict()
