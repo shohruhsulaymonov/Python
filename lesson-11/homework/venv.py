9102f22f83ea5438d033bf140b11903c1640147e
Reinhardt <shsulaymonov2007@gmail.com>
2025-06-03 19:50:54 +0500

    Update venv.py

--- lesson-11/homework/venv.py
+++ lesson-11/homework/venv.py
@@ -1,11 +1,13 @@
 #1
-python -m venv my_env
-my_env\Scripts\activate
+python -m venv my_env # venv created
+my_env\Scripts\activate # venv activated
+
 pip install requests
 pip install random
 pip install matplotlib
+#installing the libraries
 #2
-echo > math_operations.py
+echo > math_operations.py # creating th module
 def add(a, b):
     return a+b
 
@@ -21,7 +23,7 @@ def divide(a, b):
     except ZeroDivisionError:
         return "Can't divide by zero!"
 #----------------------
-echo > string_utils.py
+echo > string_utils.py #creating the module
 
 def reverse_string(x):
     return x[::-1]
@@ -29,3 +31,22 @@ def reverse_string(x):
 def count_vowels(x):
     vowels = 'aouie'
     return sum(1 for i in x if i.lower() in vowels)
+
+#3 package and modules are created.
+from math import pi
+def calc_area(radius):
+    return round(pi*radius**2, 2)
+
+def calc_circumference(radius):
+    return round(2*pi*radius, 2)
+
+#Package and modulares are created
+#file_reader:
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        contents = file.read()
+    print(contents)
+#file_writer:
+def write_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
