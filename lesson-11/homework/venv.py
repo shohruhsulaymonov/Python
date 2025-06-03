5f67d75048113a51df7f0c9eae8e18963a64c686
Reinhardt <shsulaymonov2007@gmail.com>
2025-06-03 18:40:46 +0500

    Update venv.py

--- lesson-11/homework/venv.py
+++ lesson-11/homework/venv.py
@@ -5,3 +5,27 @@ pip install requests
 pip install random
 pip install matplotlib
 #2
+echo > math_operations.py
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    try:
+        a/b
+    except ZeroDivisionError:
+        return "Can't divide by zero!"
+#----------------------
+echo > string_utils.py
+
+def reverse_string(x):
+    return x[::-1]
+
+def count_vowels(x):
+    vowels = 'aouie'
+    return sum(1 for i in x if i.lower() in vowels)
