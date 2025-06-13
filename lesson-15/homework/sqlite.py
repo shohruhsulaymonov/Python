a9d61aa94e7e23e775af638eee871a126c91e6d0
Reinhardt <shsulaymonov2007@gmail.com>
2025-06-13 17:01:46 +0500

    Update sqlite.py

--- lesson-15/homework/sqlite.py
+++ lesson-15/homework/sqlite.py
@@ -1,7 +1,17 @@
 #1
 with sqlite3.connect('database.db') as connection:
     cursor = connection.cursor()
-    query = 'CREATE TABLE Roster(Name TEXT, Species TEXT, Age INT);'
+    create = 'CREATE TABLE Roster(Name TEXT, Species TEXT, Age INT);'
     cursor.execute('DROP TABLE IF EXISTS Roster')
-    result = cursor.execute(query)
+    result = cursor.execute(create)
 #2
+with sqlite3.connect('database.db') as connection:
+    cursor = connection.cursor()
+    insert = '''
+    INSERT INTO Roster VALUES
+    ('Benjamin Sisko', 'Human', 40),
+    ('Jadzia Dax', 'Trill', 300),
+    ('Kira Nerys', 'Bajoran', 29);
+    '''
+    cursor.execute(insert)
+#3
